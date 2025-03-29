56d69bee04037404de2497407976ade26c04a349
faisalmasrimaulana <faisalmasrimaulana@gmail.com>
2025-03-29 12:26:36 +0700

    feat: menambah fitur timer

--- GUI/Timer.Designer.cs
+++ GUI/Timer.Designer.cs
@@ -29,29 +29,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.btnSave = new System.Windows.Forms.Button();
+            this.timerCountDown = new System.Windows.Forms.Timer(this.components);
             this.btnReset = new System.Windows.Forms.Button();
+            this.btnAturWaktu = new System.Windows.Forms.Button();
             this.titikDua3 = new System.Windows.Forms.Label();
             this.titikDua2 = new System.Windows.Forms.Label();
-            this.labelStopWatchJam = new System.Windows.Forms.Label();
-            this.labelStopWatchMenit = new System.Windows.Forms.Label();
+            this.labelTimerJam = new System.Windows.Forms.Label();
+            this.labelTimerMenit = new System.Windows.Forms.Label();
             this.btnMulai = new System.Windows.Forms.Button();
-            this.labelStopWatchDetik = new System.Windows.Forms.Label();
+            this.labelTimerDetik = new System.Windows.Forms.Label();
+            this.jamCount = new System.Windows.Forms.NumericUpDown();
+            this.menitCount = new System.Windows.Forms.NumericUpDown();
+            this.detikCount = new System.Windows.Forms.NumericUpDown();
+            this.jamCountLabel = new System.Windows.Forms.Label();
+            this.menitCountLabel = new System.Windows.Forms.Label();
+            this.detikCountLabel = new System.Windows.Forms.Label();
+            this.aturWaktuGroup = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.jamCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menitCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detikCount)).BeginInit();
+            this.aturWaktuGroup.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnSave
+            // timerCountDown
             // 
-            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnSave.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnSave.FlatAppearance.BorderSize = 0;
-            this.btnSave.ForeColor = System.Drawing.Color.Black;
-            this.btnSave.Location = new System.Drawing.Point(534, 278);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(133, 54);
-            this.btnSave.TabIndex = 15;
-            this.btnSave.Text = "Stop";
-            this.btnSave.UseVisualStyleBackColor = false;
+            this.timerCountDown.Interval = 1000;
+            this.timerCountDown.Tick += new System.EventHandler(this.timerCountDown_Tick);
             // 
             // btnReset
             // 
@@ -59,12 +62,27 @@
             this.btnReset.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnReset.FlatAppearance.BorderSize = 0;
             this.btnReset.ForeColor = System.Drawing.Color.Black;
-            this.btnReset.Location = new System.Drawing.Point(124, 278);
+            this.btnReset.Location = new System.Drawing.Point(534, 285);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(133, 54);
-            this.btnReset.TabIndex = 14;
-            this.btnReset.Text = "Atur Waktu";
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnAturWaktu
+            // 
+            this.btnAturWaktu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAturWaktu.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAturWaktu.FlatAppearance.BorderSize = 0;
+            this.btnAturWaktu.ForeColor = System.Drawing.Color.Black;
+            this.btnAturWaktu.Location = new System.Drawing.Point(124, 285);
+            this.btnAturWaktu.Name = "btnAturWaktu";
+            this.btnAturWaktu.Size = new System.Drawing.Size(133, 54);
+            this.btnAturWaktu.TabIndex = 14;
+            this.btnAturWaktu.Text = "Atur Waktu";
+            this.btnAturWaktu.UseVisualStyleBackColor = false;
+            this.btnAturWaktu.Click += new System.EventHandler(this.btnAturWaktu_Click);
             // 
             // titikDua3
             // 
@@ -73,7 +91,7 @@
             this.titikDua3.BackColor = System.Drawing.Color.Transparent;
             this.titikDua3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titikDua3.ForeColor = System.Drawing.Color.White;
-            this.titikDua3.Location = new System.Drawing.Point(250, 119);
+            this.titikDua3.Location = new System.Drawing.Point(250, 126);
             this.titikDua3.Name = "titikDua3";
             this.titikDua3.Size = new System.Drawing.Size(80, 116);
             this.titikDua3.TabIndex = 13;
@@ -87,38 +105,38 @@
             this.titikDua2.BackColor = System.Drawing.Color.Transparent;
             this.titikDua2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titikDua2.ForeColor = System.Drawing.Color.White;
-            this.titikDua2.Location = new System.Drawing.Point(456, 119);
+            this.titikDua2.Location = new System.Drawing.Point(456, 126);
             this.titikDua2.Name = "titikDua2";
             this.titikDua2.Size = new System.Drawing.Size(80, 116);
             this.titikDua2.TabIndex = 12;
             this.titikDua2.Text = ":";
             this.titikDua2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // labelStopWatchJam
+            // labelTimerJam
             // 
-            this.labelStopWatchJam.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.labelStopWatchJam.AutoSize = true;
-            this.labelStopWatchJam.BackColor = System.Drawing.Color.Transparent;
-            this.labelStopWatchJam.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelStopWatchJam.ForeColor = System.Drawing.Color.White;
-            this.labelStopWatchJam.Location = new System.Drawing.Point(114, 119);
-            this.labelStopWatchJam.Name = "labelStopWatchJam";
-            this.labelStopWatchJam.Size = new System.Drawing.Size(167, 116);
-            this.labelStopWatchJam.TabIndex = 11;
-            this.labelStopWatchJam.Text = "00";
+            this.labelTimerJam.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTimerJam.AutoSize = true;
+            this.labelTimerJam.BackColor = System.Drawing.Color.Transparent;
+            this.labelTimerJam.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimerJam.ForeColor = System.Drawing.Color.White;
+            this.labelTimerJam.Location = new System.Drawing.Point(114, 126);
+            this.labelTimerJam.Name = "labelTimerJam";
+            this.labelTimerJam.Size = new System.Drawing.Size(167, 116);
+            this.labelTimerJam.TabIndex = 11;
+            this.labelTimerJam.Text = "00";
             // 
-            // labelStopWatchMenit
+            // labelTimerMenit
             // 
-            this.labelStopWatchMenit.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.labelStopWatchMenit.AutoSize = true;
-            this.labelStopWatchMenit.BackColor = System.Drawing.Color.Transparent;
-            this.labelStopWatchMenit.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelStopWatchMenit.ForeColor = System.Drawing.Color.White;
-            this.labelStopWatchMenit.Location = new System.Drawing.Point(312, 119);
-            this.labelStopWatchMenit.Name = "labelStopWatchMenit";
-            this.labelStopWatchMenit.Size = new System.Drawing.Size(167, 116);
-            this.labelStopWatchMenit.TabIndex = 10;
-            this.labelStopWatchMenit.Text = "00";
+            this.labelTimerMenit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTimerMenit.AutoSize = true;
+            this.labelTimerMenit.BackColor = System.Drawing.Color.Transparent;
+            this.labelTimerMenit.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimerMenit.ForeColor = System.Drawing.Color.White;
+            this.labelTimerMenit.Location = new System.Drawing.Point(312, 126);
+            this.labelTimerMenit.Name = "labelTimerMenit";
+            this.labelTimerMenit.Size = new System.Drawing.Size(167, 116);
+            this.labelTimerMenit.TabIndex = 10;
+            this.labelTimerMenit.Text = "00";
             // 
             // btnMulai
             // 
@@ -126,42 +144,142 @@
             this.btnMulai.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnMulai.FlatAppearance.BorderSize = 0;
             this.btnMulai.ForeColor = System.Drawing.Color.Black;
-            this.btnMulai.Location = new System.Drawing.Point(332, 278);
+            this.btnMulai.Location = new System.Drawing.Point(332, 285);
             this.btnMulai.Name = "btnMulai";
             this.btnMulai.Size = new System.Drawing.Size(133, 54);
             this.btnMulai.TabIndex = 9;
             this.btnMulai.Text = "Mulai";
             this.btnMulai.UseVisualStyleBackColor = false;
+            this.btnMulai.Click += new System.EventHandler(this.btnMulai_Click);
             // 
-            // labelStopWatchDetik
+            // labelTimerDetik
             // 
-            this.labelStopWatchDetik.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.labelStopWatchDetik.AutoSize = true;
-            this.labelStopWatchDetik.BackColor = System.Drawing.Color.Transparent;
-            this.labelStopWatchDetik.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelStopWatchDetik.ForeColor = System.Drawing.Color.White;
-            this.labelStopWatchDetik.Location = new System.Drawing.Point(520, 119);
-            this.labelStopWatchDetik.Name = "labelStopWatchDetik";
-            this.labelStopWatchDetik.Size = new System.Drawing.Size(167, 116);
-            this.labelStopWatchDetik.TabIndex = 8;
-            this.labelStopWatchDetik.Text = "00";
+            this.labelTimerDetik.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTimerDetik.AutoSize = true;
+            this.labelTimerDetik.BackColor = System.Drawing.Color.Transparent;
+            this.labelTimerDetik.Font = new System.Drawing.Font("Arial Rounded MT Bold", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimerDetik.ForeColor = System.Drawing.Color.White;
+            this.labelTimerDetik.Location = new System.Drawing.Point(520, 126);
+            this.labelTimerDetik.Name = "labelTimerDetik";
+            this.labelTimerDetik.Size = new System.Drawing.Size(167, 116);
+            this.labelTimerDetik.TabIndex = 8;
+            this.labelTimerDetik.Text = "00";
+            // 
+            // jamCount
+            // 
+            this.jamCount.AccessibleName = "";
+            this.jamCount.Location = new System.Drawing.Point(94, 7);
+            this.jamCount.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.jamCount.Name = "jamCount";
+            this.jamCount.Size = new System.Drawing.Size(71, 26);
+            this.jamCount.TabIndex = 16;
+            // 
+            // menitCount
+            // 
+            this.menitCount.AccessibleName = "";
+            this.menitCount.Location = new System.Drawing.Point(94, 39);
+            this.menitCount.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.menitCount.Name = "menitCount";
+            this.menitCount.Size = new System.Drawing.Size(71, 26);
+            this.menitCount.TabIndex = 17;
+            // 
+            // detikCount
+            // 
+            this.detikCount.AccessibleName = "";
+            this.detikCount.Location = new System.Drawing.Point(94, 71);
+            this.detikCount.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.detikCount.Name = "detikCount";
+            this.detikCount.Size = new System.Drawing.Size(71, 26);
+            this.detikCount.TabIndex = 18;
+            // 
+            // jamCountLabel
+            // 
+            this.jamCountLabel.AccessibleName = "";
+            this.jamCountLabel.AutoSize = true;
+            this.jamCountLabel.BackColor = System.Drawing.Color.Transparent;
+            this.jamCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.jamCountLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.jamCountLabel.Location = new System.Drawing.Point(38, 7);
+            this.jamCountLabel.Name = "jamCountLabel";
+            this.jamCountLabel.Size = new System.Drawing.Size(46, 22);
+            this.jamCountLabel.TabIndex = 19;
+            this.jamCountLabel.Text = "Jam";
+            // 
+            // menitCountLabel
+            // 
+            this.menitCountLabel.AccessibleName = "";
+            this.menitCountLabel.AutoSize = true;
+            this.menitCountLabel.BackColor = System.Drawing.Color.Transparent;
+            this.menitCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.menitCountLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.menitCountLabel.Location = new System.Drawing.Point(26, 39);
+            this.menitCountLabel.Name = "menitCountLabel";
+            this.menitCountLabel.Size = new System.Drawing.Size(58, 22);
+            this.menitCountLabel.TabIndex = 20;
+            this.menitCountLabel.Text = "Menit";
+            // 
+            // detikCountLabel
+            // 
+            this.detikCountLabel.AccessibleName = "";
+            this.detikCountLabel.AutoSize = true;
+            this.detikCountLabel.BackColor = System.Drawing.Color.Transparent;
+            this.detikCountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.detikCountLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.detikCountLabel.Location = new System.Drawing.Point(30, 71);
+            this.detikCountLabel.Name = "detikCountLabel";
+            this.detikCountLabel.Size = new System.Drawing.Size(56, 22);
+            this.detikCountLabel.TabIndex = 21;
+            this.detikCountLabel.Text = "Detik";
+            // 
+            // aturWaktuGroup
+            // 
+            this.aturWaktuGroup.Controls.Add(this.menitCountLabel);
+            this.aturWaktuGroup.Controls.Add(this.detikCount);
+            this.aturWaktuGroup.Controls.Add(this.detikCountLabel);
+            this.aturWaktuGroup.Controls.Add(this.jamCountLabel);
+            this.aturWaktuGroup.Controls.Add(this.jamCount);
+            this.aturWaktuGroup.Controls.Add(this.menitCount);
+            this.aturWaktuGroup.Location = new System.Drawing.Point(94, 345);
+            this.aturWaktuGroup.Name = "aturWaktuGroup";
+            this.aturWaktuGroup.Size = new System.Drawing.Size(200, 100);
+            this.aturWaktuGroup.TabIndex = 23;
+            this.aturWaktuGroup.Visible = false;
             // 
             // Timer_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnSave);
+            this.ClientSize = new System.Drawing.Size(800, 465);
+            this.Controls.Add(this.aturWaktuGroup);
             this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAturWaktu);
             this.Controls.Add(this.titikDua3);
             this.Controls.Add(this.titikDua2);
-            this.Controls.Add(this.labelStopWatchJam);
-            this.Controls.Add(this.labelStopWatchMenit);
+            this.Controls.Add(this.labelTimerJam);
+            this.Controls.Add(this.labelTimerMenit);
             this.Controls.Add(this.btnMulai);
-            this.Controls.Add(this.labelStopWatchDetik);
+            this.Controls.Add(this.labelTimerDetik);
+            this.ForeColor = System.Drawing.Color.Transparent;
             this.Name = "Timer_form";
             this.Text = "Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.jamCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.menitCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detikCount)).EndInit();
+            this.aturWaktuGroup.ResumeLayout(false);
+            this.aturWaktuGroup.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,14 +287,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Timer timerCountDown;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnAturWaktu;
         private System.Windows.Forms.Label titikDua3;
         private System.Windows.Forms.Label titikDua2;
-        private System.Windows.Forms.Label labelStopWatchJam;
-        private System.Windows.Forms.Label labelStopWatchMenit;
+        private System.Windows.Forms.Label labelTimerJam;
+        private System.Windows.Forms.Label labelTimerMenit;
         private System.Windows.Forms.Button btnMulai;
-        private System.Windows.Forms.Label labelStopWatchDetik;
+        private System.Windows.Forms.Label labelTimerDetik;
+        private System.Windows.Forms.NumericUpDown jamCount;
+        private System.Windows.Forms.NumericUpDown menitCount;
+        private System.Windows.Forms.NumericUpDown detikCount;
+        private System.Windows.Forms.Label jamCountLabel;
+        private System.Windows.Forms.Label menitCountLabel;
+        private System.Windows.Forms.Label detikCountLabel;
+        private System.Windows.Forms.Panel aturWaktuGroup;
     }
 }
--- GUI/Timer.cs
+++ GUI/Timer.cs
@@ -10,46 +10,117 @@ using System.Windows.Forms;
 
 namespace StopWatchC_
 {
-    public partial class Timer_form: Form
+    public partial class Timer_form : Form
     {
+        //inisialisasi jam, menit, dan detik
+        int jam;
+        int menit;
+        int detik;
         public Timer_form()
         {
             InitializeComponent();
         }
 
-        private void btnReset_Click(object sender, EventArgs e)
+
+        private void btnAturWaktu_Click(object sender, EventArgs e)
         {
+            
+            if (aturWaktuGroup.Visible == false)
+            {
+                aturWaktuGroup.Visible = true;
+            }
+            //jika sudah muncul, dan user sudah mengatur waktu, maka valuenya akan dimasukkan ke timerlabel
+            else if (aturWaktuGroup.Visible == true)
+            {
+                //ambil value pada jamCount dan masukkan ke variabel jam
+                jam = int.Parse(jamCount.Text);
+                menit = int.Parse(menitCount.Text);
+                detik = int.Parse(detikCount.Text);
+                //masukkan value jam,menit,dan detik ke labelstopwatch
+                labelTimerJam.Text = jam.ToString("D2");
+                labelTimerMenit.Text = menit.ToString("D2");
+                labelTimerDetik.Text = detik.ToString("D2");
 
-        }
-
-        private void btnSave_Click(object sender, EventArgs e)
-        {
-
+                aturWaktuGroup.Visible = false;
+            }
         }
 
         private void btnMulai_Click(object sender, EventArgs e)
         {
 
+            if (timerCountDown.Enabled)
+            {
+                timerCountDown.Stop();
+                btnMulai.Text = "Mulai";
+            }
+            else
+            //kondisi agar menghindari error ketika tombol klik ditekan
+            {
+                //jika waktu masih 0, maka akan muncul perintah untuk mengatur waktunya
+                if (jam == 0 && menit == 0 && detik == 0 || aturWaktuGroup.Visible == true)
+                {
+                        MessageBox.Show("Atur Waktu terlebih dahulu");
+                   
+                }
+                //jika waktu sudah diatur maka timer dimulai dan tulisan tombol mulai akan berubah menjadi berhenti
+                else if (aturWaktuGroup.Visible == false)
+                {
+                timerCountDown.Start();
+                btnMulai.Text = "Berhenti";
+                }
+            }
         }
 
-        private void labelStopWatchJam_Click(object sender, EventArgs e)
+        private void timerCountDown_Tick(object sender, EventArgs e)
         {
-
+            if (timerCountDown.Enabled)
+            {
+                //jika detik sama dengan 0, maka menit berkurang, tapi menit, detik, jam tidak boleh sampai negatif
+                if (jam == 0 && menit == 0 && detik == 0)
+                {
+                    timerCountDown.Stop();
+                    MessageBox.Show("Waktu Habis");
+                    btnMulai.Text = "Mulai";
+                    jam = 0;
+                    menit = 0;
+                    detik = 0;
+                }
+                if (detik > 0)
+                {
+                    detik--;
+                }
+                
+                else{
+                    if (menit > 0) {
+                        menit--;
+                        detik = 59;
+                    }
+                   else if (jam > 0){
+                        jam--;
+                        menit = 59;
+                        detik = 59;
+                    }
+                }
+                labelTimerDetik.Text = detik.ToString("D2");
+                labelTimerMenit.Text = menit.ToString("D2");
+                labelTimerJam.Text = jam.ToString("D2");
+            }
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void btnReset_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
+            if (!timerCountDown.Enabled)
+            {
+                detik = 0;
+                menit = 0;
+                jam = 0;
+                labelTimerDetik.Text = "00";
+                labelTimerMenit.Text = "00";
+                labelTimerJam.Text = "00";
+                jamCount.Text = "0";
+                menitCount.Text = "0";
+                detikCount.Text = "0";
+            }
         }
     }
 }
